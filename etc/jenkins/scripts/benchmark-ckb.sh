8dc3a54ef9d23fc30b4d5b8697739a25f228a763
Boyu Yang <yangby@cryptape.com>
2020-03-20 13:42:47 +0800

    feat: save all logs in the ci server

--- etc/jenkins/scripts/benchmark-ckb.sh
+++ etc/jenkins/scripts/benchmark-ckb.sh
@@ -5,6 +5,8 @@ set -euo pipefail
 currdir="$(pwd)"
 workdir="${currdir}/workdir"
 
+HAS_LOGS=false
+
 function github_comment () {
     local message="${1}"
     if [ -n "${ghprbPullId}" ] && [ -n "${ghprbGhRepository}" ]; then
@@ -16,10 +18,24 @@ function github_comment () {
 }
 
 function exit_func () {
+    set +e
     if [ -d "${workdir}" ]; then
         cd "${workdir}"
+        if [ "${HAS_LOGS}" = "true" ]; then
+            ./bin/ckb-ci bench logs
+        fi
         ./bin/ckb-ci free
         rm etc/terraform/terraform.tfvars
+        if [ -d "ckb-logs" ]; then
+            local timestamp=$(date +"%Y%m%d%H%M%S")
+            local version=$(echo "${CKB_VERSION}" | cut -c 1-7)
+            local log_id="ckb-logs-${version}-${timestamp}"
+            mv "ckb-logs" "${log_id}"
+            tar -czvf "${log_id}.tgz" "${log_id}"
+            rm -rf "${log_id}"
+            mkdir -p "${currdir}/ckb-logs"
+            mv "${log_id}.tgz" "${currdir}/ckb-logs/"
+        fi
     fi
 }
 
@@ -44,6 +60,7 @@ function main () {
     # example: 2in2out, mixed
     echo "[INFO] Bench Type = [${BENCH_TYPE}]"
     echo "[INFO] Expected Samples Count = [${EXPECTED_SAMPLES_COUNT}]"
+    echo "[INFO] CKB Logger Filter = [${CKB_LOGGER_FILTER}]"
 
     export CKB_CI_NOCOLOR=true
 
@@ -57,21 +74,30 @@ function main () {
     cp -r /mnt/workdir/bin ./
     cp -r /mnt/workdir/etc ./
 
+    local terraform_tfvars="etc/terraform/terraform.tfvars"
     local instances_count=$((INSTANCES_COUNT - 1))
-    echo "access_key      = \"${AWS_ACCESS_KEY}\"" >  etc/terraform/terraform.tfvars
-    echo "secret_key      = \"${AWS_SECRET_KEY}\"" >> etc/terraform/terraform.tfvars
-    echo "instances_count = ${instances_count}"    >> etc/terraform/terraform.tfvars
-    echo "instance_type   = \"${INSTANCE_TYPE}\""  >> etc/terraform/terraform.tfvars
+    echo "access_key      = \"${AWS_ACCESS_KEY}\"" >  "${terraform_tfvars}"
+    echo "secret_key      = \"${AWS_SECRET_KEY}\"" >> "${terraform_tfvars}"
+    echo "instances_count = ${instances_count}"    >> "${terraform_tfvars}"
+    echo "instance_type   = \"${INSTANCE_TYPE}\""  >> "${terraform_tfvars}"
 
     ./bin/ckb-ci bench init
 
-    echo "[all:vars]"                                       >> etc/ansible/hosts
-    echo "ckb_ghrepo=${CKB_GHREPO}"                         >> etc/ansible/hosts
-    echo "ckb_version=${CKB_VERSION}"                       >> etc/ansible/hosts
-    echo "bench_type=${BENCH_TYPE}"                         >> etc/ansible/hosts
-    echo "expected_samples_count=${EXPECTED_SAMPLES_COUNT}" >> etc/ansible/hosts
+    local ansible_hosts="etc/ansible/hosts"
+    echo "[all:vars]"                                       >> "${ansible_hosts}"
+    echo "ckb_ghrepo=${CKB_GHREPO}"                         >> "${ansible_hosts}"
+    echo "ckb_version=${CKB_VERSION}"                       >> "${ansible_hosts}"
+    echo "bench_type=${BENCH_TYPE}"                         >> "${ansible_hosts}"
+    echo "expected_samples_count=${EXPECTED_SAMPLES_COUNT}" >> "${ansible_hosts}"
+    echo "ckb_logger_filter=${CKB_LOGGER_FILTER}"           >> "${ansible_hosts}"
+
+    echo "[INFO] Review the ansible hosts file:"
+    cat "${ansible_hosts}"
 
     ./bin/ckb-ci bench setup
+
+    HAS_LOGS=true
+
     ./bin/ckb-ci bench prepare
     ./bin/ckb-ci bench run
     ./bin/ckb-ci bench result
@@ -88,8 +114,15 @@ function main () {
         | paste -sd+\
     )
     local tps=$(echo "scale=2; (${tps_sum_tmp}) / ${count}" | bc)
-    local result=$(printf "#### Benchmark Result\\\\r  - TPS = %s\\\\r  - Samples Count = %s\\\\r  - CKB Version = %s\\\\r  - Instance Type = %s\\\\r  - Instances Count = %s\\\\r  - Bench Type = %s\\\\r" \
-        "${tps}" "${count}" "${CKB_VERSION}" "${INSTANCE_TYPE}" "${INSTANCES_COUNT}" "${BENCH_TYPE}")
+
+    local result="#### Benchmark Result"
+    result="${result}\\r  - TPS: ${tps}"
+    result="${result}\\r  - Samples Count: ${count}"
+    result="${result}\\r  - CKB Version: ${CKB_VERSION}"
+    result="${result}\\r  - Instance Type: ${INSTANCE_TYPE}"
+    result="${result}\\r  - Instances Count: ${INSTANCES_COUNT}"
+    result="${result}\\r  - Bench Type: ${BENCH_TYPE}"
+    result="${result}\\r  - CKB Logger Filter: ${CKB_LOGGER_FILTER}"
     echo "[INFO] ${result}"
     github_comment "${result}"
 }
